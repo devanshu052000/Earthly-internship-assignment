20d4682235bc5889a94879b75f660fd4d529f9a7
TheObjectOrientedGuy <73549716+TheObjectOrientedGuy@users.noreply.github.com>
2023-02-09 17:35:27 +0530

    final commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDe59oZBaNFSWtfGkozpo7xwJ4NlxjHgwU",
+    authDomain: "earthly-sample.firebaseapp.com",
+    projectId: "earthly-sample",
+    storageBucket: "earthly-sample.appspot.com",
+    messagingSenderId: "844078614763",
+    appId: "1:844078614763:web:a17f4ade316abe9d9216ae",
+    measurementId: "G-R03NE4NQJN"
+  };
